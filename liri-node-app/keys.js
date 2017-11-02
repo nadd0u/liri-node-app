153a2f050152bcfc845e8c8eb913b297f90d0e47
nadianm <nadia.nouimehidi@gmail.com>
2017-11-01 21:49:19 -0400

    Add files via upload

---
+++ liri-node-app/keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '  5Z6r5HpCNVr2qgNS1Str5C57J',
+  consumer_secret: 'yFlH1EHINc7sbRqFFJIkDWCapmNaYU1ZX86DaUDoZnHKn9JQ0g',
+  access_token_key: '124847769-Pc9JUCQz8qW4ASKhq16Ff4tzt4vElO68CJ7eRka3',
+
+  access_token_secret: 'X5wzjPHfYICB8FpNJpmPMZYUNm3mfAxLFjNSyVb04pexf',
+
+}
+exports.omdb_api = '40e9cece';
+
+exports.spotifyKeys = {
+    id: '38793e87171b4840b590338266470d78',
+    secret: '893e64131a3843938e1e479c7cce93c2'
+};
